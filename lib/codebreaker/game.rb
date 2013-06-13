6f4280bfa585f02cb9d21027361ebf19e4136b9a
Danish Satkut <danish_satkut@hotmail.com>
2013-06-13 18:02:11 +0530

    Refactored the guess method for codebreaker submits feature.

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -12,35 +12,35 @@ module Codebreaker
     end
 
     def guess(guess)
-      number_match = exact_match = 0
-      
-      guess.chars.each_with_index do |number, index|
-        if exact_match?(guess, index)
-          exact_match += 1
-        elsif number_match?(guess, index)
-          number_match += 1
-        end
-      end
-      
-      @output.puts generate_mark(exact_match, number_match)
+      @output.puts ('+' * exact_match_count(guess)) + ('-' * number_match_count(guess))
     end
 
     private
 
+      GUESS_DIGIT_COUNT = 4
+
+      # Check whether the number at the position is exact match
       def exact_match?(guess, position)
         guess[position] === @secret[position]
       end
 
+      # Check whether the number at the position is a number match
       def number_match?(guess, position)
-        @secret.include?(guess[position])
+        @secret.include?(guess[position]) && !exact_match?(guess, position)
       end
 
-      # generates the mark signs depending on exact and number match
-      def generate_mark(exact_match, number_match)
-        mark = ''
-        exact_match.times { mark += '+' }
-        number_match.times { mark += '-' }  
-        return mark
+      # Count exact matches
+      def exact_match_count(guess)
+        (0...GUESS_DIGIT_COUNT).inject(0) do |count, index|
+          exact_match?(guess, index) ? count += 1 : count
+        end
+      end
+
+      # Count number matches
+      def number_match_count(guess)
+        (0...GUESS_DIGIT_COUNT).inject(0) do |count, index|
+          number_match?(guess, index) ? count += 1 : count
+        end
       end
   end
 end
